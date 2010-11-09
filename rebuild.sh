9ac9afcf4194081b23f549a597bfe9faddb980d8
jamezilla <james@virtualjames.com>
2010-11-09 14:07:02 -0800

    minor cleanups of docs

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gem build bcms_pubcookie.gemspec --verbose
-gem install bcms_pubcookie-1.0.0.gem 
+gem install bcms_pubcookie-0.1.0.gem 
 sudo apachectl restart
 #tail -f ~/src/dxarts/log/development.log 
